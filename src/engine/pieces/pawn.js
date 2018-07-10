0d1a04d549c0a96089594b2f1cf7dd104ac3d6b3
Leo Zlotnikov <leonid.zlotnikov@softwire.com>
2018-07-10 12:20:16 +0100

    Pawns take pieces diagonally

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -20,6 +20,14 @@ export default class Pawn extends Piece {
                 moves.push(Square.at(square2InFront.row, square2InFront.col));
             }
         }
+
+        [1, -1].forEach(dCol => {
+            let checkCapture = Square.at(square.row + dir, square.col + dCol);
+            if(board.isCapturable(checkCapture, this.player)) {
+                moves.push(checkCapture);
+            }
+        });
+
         return moves.filter(sq => sq.isOnBoard());
     }
 }
