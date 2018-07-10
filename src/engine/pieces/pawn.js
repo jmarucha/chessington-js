90b261c24978337737794a6f8a6ee3611a958b6a
Jan Marucha <jan.jarucha@softwire.com>
2018-07-10 09:51:19 +0100

    pawns can move

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Player from '../player';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +8,15 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
+        let square = board.findPiece(this);
+
+        if (this.player === Player.WHITE) {
+            return [Square.at(square.row + 1, square.col)];
+        }
+        if (this.player === Player.BLACK) {
+            return [Square.at(square.row - 1, square.col)];
+        }
+
         return new Array(0);
     }
 }
